45a3418950b28698c8d14cc4e24ae0999f92f830
Bernardo Figueiredo <bernardopascoalfigueiredo@tecnico.ulisboa.pt>
2015-03-16 15:19:55 +0000

    Fixed function declaration node to have a return type and a function name instead of a variable node

--- ast/function_node.h
+++ ast/function_node.h
@@ -6,6 +6,7 @@
 #include "variable_node.h"
 #include "cdk/basic_type.h"
 #include <vector>
+#include <string>
 
 
 namespace pwn {
@@ -14,20 +15,30 @@ namespace pwn {
    * Class for describing a function declaration (ex: #argc(), #fibonacci(#n) ) and possible implementation
    */
   class function_node: public cdk::basic_node {
-    pwn::variable_node *_function; /* function name and return type*/
+    basic_type *_return_type;
+    std::string _function;
     typedef std::vector<cdk::expression_node *> parameters_t;
     parameters_t *_parameters; /*empty for functions without arguments*/
     typedef std::vector<cdk::basic_node *> body_t;
     body_t *_body; /*optional for declaration*/
 
   public:
-    inline function_node(int lineno, pwn::variable_node *function, parameters_t *parameters =nullptr, body_t *body =nullptr) :
-        cdk::basic_node(lineno), _function(function), _parameters(parameters), _body(body) {
+    inline function_node(int lineno, basic_type *return_type, const std::string *function, parameters_t *parameters =nullptr, body_t *body =nullptr) :
+        cdk::basic_node(lineno), _return_type(return_type), _function(*function), _parameters(parameters), _body(body) {
+    }
+    inline function_node(int lineno, basic_type *return_type, const std::string &function, parameters_t *parameters =nullptr, body_t *body =nullptr) :
+        cdk::basic_node(lineno), _return_type(return_type), _function(function), _parameters(parameters), _body(body) {
+    }
+    inline function_node(int lineno, basic_type *return_type, const char *function, parameters_t *parameters =nullptr, body_t *body =nullptr) :
+        cdk::basic_node(lineno), _return_type(return_type), _function(function), _parameters(parameters), _body(body) {
     }
 
   public:
-    inline pwn::variable_node *function() {
-      return _function;
+    inline basic_type *return_type() {
+      return _return_type;
+    }
+    inline std::string *function() {
+      return &_function;
     }
     inline parameters_t *parameters() {
       return _parameters;
