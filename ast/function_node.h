59621543fe9d1ace5e92c98793e3f67aeb43bcf9
Bernardo Figueiredo <bernardopascoalfigueiredo@tecnico.ulisboa.pt>
2015-03-18 16:35:35 +0000

    Fixed the function node so it uses sequence_node. also added a bool for the import. also added a expression_node for the default return value(fix later)

--- ast/function_node.h
+++ ast/function_node.h
@@ -2,8 +2,7 @@
 #define __PWN_FUNCTIONNODE_H__
 
 #include "cdk/ast/basic_node.h"
-#include "cdk/ast/expression_node.h"
-#include "variable_node.h"
+#include "cdk/ast/sequence_node.h"
 #include "cdk/basic_type.h"
 #include <vector>
 #include <string>
@@ -15,35 +14,41 @@ namespace pwn {
    * Class for describing a function declaration (ex: #argc(), #fibonacci(#n) ) and possible implementation
    */
   class function_node: public cdk::basic_node {
+    bool _import;
     basic_type *_return_type;
     std::string _function;
-    typedef std::vector<cdk::expression_node *> parameters_t;
-    parameters_t *_parameters; /*empty for functions without arguments*/
-    typedef std::vector<cdk::basic_node *> body_t;
-    body_t *_body; /*optional for declaration*/
+    cdk::expression_node *_default_return; /*default return value if it exists. it shouldn't be implemented as an expression, correct later*/
+    cdk::sequence_node *_parameters; /*empty for functions without arguments*/
+    cdk::sequence_node *_body; /*optional for declaration*/
 
   public:
-    inline function_node(int lineno, basic_type *return_type, const std::string *function, parameters_t *parameters =nullptr, body_t *body =nullptr) :
-        cdk::basic_node(lineno), _return_type(return_type), _function(*function), _parameters(parameters), _body(body) {
+    inline function_node(int lineno, basic_type *return_type, bool import, const std::string *function, cdk::expression_node *default_return, cdk::sequence_node *parameters, cdk::sequence_node *body) :
+        cdk::basic_node(lineno), _import(import), _return_type(return_type), _function(*function), _default_return(default_return), _parameters(parameters), _body(body) {
     }
-    inline function_node(int lineno, basic_type *return_type, const std::string &function, parameters_t *parameters =nullptr, body_t *body =nullptr) :
-        cdk::basic_node(lineno), _return_type(return_type), _function(function), _parameters(parameters), _body(body) {
+    inline function_node(int lineno, basic_type *return_type, bool import, const std::string &function, cdk::expression_node *default_return, cdk::sequence_node *parameters, cdk::sequence_node *body) :
+        cdk::basic_node(lineno), _import(import), _return_type(return_type), _function(function), _default_return(default_return), _parameters(parameters), _body(body) {
     }
-    inline function_node(int lineno, basic_type *return_type, const char *function, parameters_t *parameters =nullptr, body_t *body =nullptr) :
-        cdk::basic_node(lineno), _return_type(return_type), _function(function), _parameters(parameters), _body(body) {
+    inline function_node(int lineno, basic_type *return_type, bool import, const char *function, cdk::expression_node *default_return, cdk::sequence_node *parameters, cdk::sequence_node *body) :
+        cdk::basic_node(lineno), _import(import), _return_type(return_type), _function(function), _default_return(default_return), _parameters(parameters), _body(body) {
     }
 
   public:
+    inline bool import() {
+      return _import;
+    }
     inline basic_type *return_type() {
       return _return_type;
     }
     inline std::string *function() {
       return &_function;
     }
-    inline parameters_t *parameters() {
+    inline cdk::expression_node *default_return() {
+      return _default_return;
+    }
+    inline cdk::sequence_node *parameters() {
       return _parameters;
     }
-    inline body_t *body() {
+    inline cdk::sequence_node *body() {
       return _body;
     }
 
