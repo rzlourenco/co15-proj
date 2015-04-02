6cc6cb11c43e47b0ad92ba670c423c281d265fc8
Bernardo Figueiredo <bernardopascoalfigueiredo@tecnico.ulisboa.pt>
2015-04-02 14:51:37 +0100

    pwn_type.h

---
+++ pwn_type.h
@@ -0,0 +1,11 @@
+#ifndef __PWNTYPE_H__
+#define __PWNTYPE_H__
+
+#include <basic_type.h>
+
+struct pwn_type {
+  typedef basic_type::type type;
+  static const type TYPE_CONST = 1UL<<29;
+};
+
+#endif
