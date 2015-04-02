bda590b1c8da124e4b375c80d2dbbd2a1d33ef81
Bernardo Figueiredo <bernardopascoalfigueiredo@tecnico.ulisboa.pt>
2015-04-02 12:15:38 +0100

    fixed return node that shouldn't have a return value

--- ast/return_node.h
+++ ast/return_node.h
@@ -10,14 +10,10 @@ namespace pwn {
    * Class for describing the return keyword
    */
   class return_node: public cdk::basic_node {
-    cdk::expression_node *_return_value;
+    
   public:
-    inline return_node(int lineno, cdk::expression_node *return_value) :
-        cdk::basic_node(lineno), _return_value(return_value)  {
-    }
-
-    inline cdk::expression_node *return_value() {
-      return _return_value;
+    inline return_node(int lineno) :
+        cdk::basic_node(lineno)  {
     }
 
     void accept(basic_ast_visitor *sp, int level) {
