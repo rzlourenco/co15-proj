8fa1a0168a9b0f6f85db0e402f3c416b66280162
Rodrigo Lourenço <guigo.lourenco@gmail.com>
2015-05-19 13:36:12 +0100

    faz-me um filho, Pestana!

---
+++ tests-pwn-daily-201504081911/run-all.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+mkdir -p .o asm exe actual
+
+for src in *.pwn; do
+    bname="$(basename -s .pwn "$src")"
+    asmfile="asm/$bname.asm"
+    ofile=".o/$bname.o"
+
+    echo "Compiling $bname"
+    ../pwn -g "$src" -o "$asmfile"
+    yasm -felf32 "$asmfile" -o "$ofile" || continue;
+    ld -m elf_i386 -L"$HOME/compiladores/root/usr/lib" "$ofile" -lrts -o "exe/$bname" || continue;
+    
+    "exe/$bname" > "actual/$bname.out" || continue;
+    diff --ignore-all-space "expected/$bname.out" "actual/$bname.out" 1>/dev/null && continue;
+    echo "$bname did not pass the tests"
+done
+
+rm -r .o
+
